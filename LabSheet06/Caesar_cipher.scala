fdc76c49c76269f4c6e2e3f8f1829276a0c7b877
Yuhanga Induwara <121022094+YuhangaInduwara@users.noreply.github.com>
2023-07-24 11:27:50 +0530

    Update Caesar_cipher.scala

--- LabSheet06/Caesar_cipher.scala
+++ LabSheet06/Caesar_cipher.scala
@@ -1,44 +1,46 @@
 object Caesar_cipher {
 
-  def encryption(string: String, count:Int):String = {
-    var array: Array[Char]= string.toCharArray;
-    for(i <- 0 until (string.length - 1)){
-      if(array(i).toInt == 32){
-      }
-      else{
-        if ((array(i).toInt >= 65 && array(i).toInt <= 90) || (array(i).toInt >= 97 && array(i).toInt <= 122)) {
-          array(i) = (array(i).toInt + count).toChar;
+  def encryption(string: String, count: Int): String = {
+    var array: Array[Char] = string.toCharArray
+    for (i <- 0 until string.length) {
+      if (array(i).toInt == 32) {
+      } else {
+        if (array(i).isUpper) {
+          array(i) = ((array(i).toInt + count - 'A'.toInt) % 26 + 'A'.toInt).toChar
+        } else if (array(i).isLower) {
+          array(i) = ((array(i).toInt + count - 'a'.toInt) % 26 + 'a'.toInt).toChar
         }
       }
     }
-    array.mkString;
+    array.mkString
   }
 
   def decryption(string: String, count: Int): String = {
-    var array: Array[Char]= string.toCharArray;
-    for (i <- 0 until (string.length - 1)) {
+    var array: Array[Char] = string.toCharArray
+    for (i <- 0 until string.length) {
       if (array(i).toInt == 32) {
-      }
-      else {
-        if ((array(i).toInt >= (65 + count) && array(i).toInt <= (90 + count)) || (array(i).toInt >= (97 + count) && array(i).toInt <= (122 + count))) {
-          array(i) = (array(i).toInt - count).toChar;
+      } else {
+        if (array(i).isUpper) {
+          array(i) = ((array(i).toInt - count - 'A'.toInt + 26) % 26 + 'A'.toInt).toChar
+        } else if (array(i).isLower) {
+          array(i) = ((array(i).toInt - count - 'a'.toInt + 26) % 26 + 'a'.toInt).toChar
         }
       }
     }
-    array.mkString;
+    array.mkString
   }
 
-  def  caesar_cipher(string: String, count: Int, function: (String,Int) => String): String = {
-    function(string, count);
+  def caesar_cipher(string: String, count: Int, function: (String, Int) => String): String = {
+    function(string, count % 26)
   }
 
   def main(args: Array[String]): Unit = {
-    var count = 10;
-    var string = "The American Standard Code for Information Interchange (ASCII) was developed under the auspices of a committee of the American Standards Association (ASA),";
-    println("Text to encrypt: " + string);
-    var encrypted_text = caesar_cipher(string, count, encryption);
+    var count = 10
+    var string = "The American Standard Code for Information Interchange (ASCII) was developed under the auspices of a committee of the American Standards Association (ASA),"
+    println("Text to encrypt: " + string)
+    var encrypted_text = caesar_cipher(string, count, encryption)
     var decrypted_text = caesar_cipher(encrypted_text, count, decryption)
-    println("Encrypted text: " + encrypted_text);
-    println("Decrypted text: " + decrypted_text);
+    println("Encrypted text: " + encrypted_text)
+    println("Decrypted text: " + decrypted_text)
   }
 }

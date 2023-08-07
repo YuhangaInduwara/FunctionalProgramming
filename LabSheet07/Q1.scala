d624a3d7f6338dac4ebd46a85d2aafa69681408f
Yuhanga Induwara <121022094+YuhangaInduwara@users.noreply.github.com>
2023-08-07 08:53:11 +0530

    Create Q1.scala

---
+++ LabSheet07/Q1.scala
@@ -0,0 +1,10 @@
+object Q1 {
+  def filterEvenNumbers(arr: Array[Int]): Array[Int] = {
+    var arr2 = arr.filter(x=>(x%2 == 0));
+    arr2;
+  }
+  def main(args: Array[String]): Unit = {
+    val arr: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    println(filterEvenNumbers(arr).mkString(", "));
+  }
+}

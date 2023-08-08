1a27b5d929f1fe3ec6493eaeb5203f6315ba5d68
Yuhanga Induwara <121022094+YuhangaInduwara@users.noreply.github.com>
2023-08-08 07:43:01 +0530

    Update Q3.scala

--- LabSheet07/Q3.scala
+++ LabSheet07/Q3.scala
@@ -1,22 +1,23 @@
 object Q3 {
   def filterPrime(arr: Array[Int]): Array[Int] = {
-    var arr2 = arr.filter(x => {
-      if (x <= 1) false
-      else if (x <= 3) true
-      else if (x % 2 == 0 || x % 3 == 0) false
-      else {
-        var i = 5
-        while (i * i <= x) {
-          if (x % i == 0 || x % (i + 2) == 0)
-            i += 6
-        }
-        true
-      }
-    })
-    arr2;
+    var arr2 = arr.filter(x => prime(x))
+    arr2
   }
+
+  def gcd(a: Int, b: Int): Int = b match {
+    case 0 => a
+    case b if a < b => gcd(b, a)
+    case _ => gcd(b, a % b)
+  }
+
+  def prime(a: Int, b: Int = 2): Boolean = b * b > a match {
+    case true => true
+    case false if a % b == 0 => false
+    case _ => prime(a, b + 1)
+  }
+
   def main(args: Array[String]): Unit = {
-    val arr: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-    println(filterPrime(arr).mkString(", "));
+    val arr: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 49)
+    println(filterPrime(arr).mkString(", "))
   }
 }
